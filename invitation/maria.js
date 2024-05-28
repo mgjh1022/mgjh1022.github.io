71af427c33bee2e1aa5b98621948b5235b200664
Mingyu Park <hello0922@cau.ac.kr>
2024-05-28 13:50:55 +0900

    disable DB

--- invitation/maria.js
+++
@@ -1,9 +0,0 @@
-const maria = require('mysql');
-const conn = maria.createConnection({
-    host:'localhost',
-    port:3306,
-    user:'invitation',
-    password:'Qwer1234!@',
-    database:'invitation'
-});
-module.exports = conn;
